394962712f2971071848aed0412a26d601c4ac66
Luke Seo <lukeyseo@gmail.com>
2020-02-11 16:07:28 -0800

    Code splitting

--- app/index.js
+++ app/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './index.css'
 import Nav from './components/Nav'
-import Posts from './components/Posts'
-import User from './components/User'
-import Post from './components/Post'
+import Loading from './components/Loading'
+
+const Posts = React.lazy(() => import('./components/Posts'));
+const Post = React.lazy(() => import('./components/Post'));
+const User = React.lazy(() => import('./components/User'));
+
 
 class App extends React.Component {
 
@@ -15,20 +18,22 @@ class App extends React.Component {
                 <div className="container">
                     <Nav/>
 
-                    <Switch>
-                        <Route 
-                            exact
-                            path="/"
-                            render={() => <Posts type="top"/>}
-                        />
+                    <React.Suspense fallback={<Loading/>}>
+                        <Switch>
+                            <Route 
+                                exact
+                                path="/"
+                                render={() => <Posts type="top"/>}
+                            />
 
-                        <Route 
-                            path="/new"
-                            render={() => <Posts type="new"/>}
-                        />
-                        <Route path="/user" component={User}/>
-                        <Route path="/post" component={Post}/>
-                    </Switch>
+                            <Route 
+                                path="/new"
+                                render={() => <Posts type="new"/>}
+                            />
+                            <Route path="/user" component={User}/>
+                            <Route path="/post" component={Post}/>
+                        </Switch>
+                    </React.Suspense>
                 </div>
             </Router>
         )
